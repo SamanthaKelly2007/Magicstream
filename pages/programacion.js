a4ba14607c7da13191eb43d9c6919335007c723a
Alexander Mamani <alex.rps96@gmail.com>
2022-07-09 22:47:22 -0300

    fix

--- pages/programacion.js
+++ pages/programacion.js
@@ -1,20 +1,54 @@
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
+import seriesSchedule from './api/serie.json'
+import {setTheTime} from '../utils/tools'
 import Prog_Styles from '../styles/Programacion.module.css'
 
 export default function Home() {
-
+    const [ListAM, setListAM] = useState([])
+    const [ListPM, setListPM] = useState([])
     const [tableAMPM, setTableAMPM] = useState(false)
+    let hours = new Date().getHours()
+    let minutes = new Date().getMinutes()
+    minutes = minutes > 0 && minutes < 30? "00" : "30"
+    let rightNow = hours+":"+minutes
 
     const changeSchedule = (prop) => {
         setTableAMPM(prop)
     }
 
+    const getSchedule = () =>{
+        let list = [];
+
+        for (let i = 0; i <= 48; i++) {
+            seriesSchedule.filter(serie => serie.emision_1_week === i || serie.emision_2_week === i  || serie.emision_3_week === i ).map(dato => (
+            list[i] = {
+                    id : i,
+                    nombre : dato.nombre,
+                    tiempo : setTheTime(i),
+                    nombre_weekend : "No Disponible por Ahora"
+                }
+            ))
+        }
+
+        for (let i = 0; i <= 48; i++) {
+            seriesSchedule.filter(serie => serie.emision_1_weekend === i || serie.emision_2_weekend === i || serie.emision_3_weekend === i).map(dato => (
+            list[i] = {
+                    ...list[i],
+                    nombre_weekend : dato.nombre
+                }
+            ))
+        }
+
+        return list
+    }
+
     useEffect(() => {
+        setListAM(getSchedule().slice(0,25))
+        setListPM(getSchedule().slice(25,49))
         const today = new Date(),
         time = today.getHours()
 
-        
         return time < 12 ? setTableAMPM(false) : setTableAMPM(true)
     }, [])
 
@@ -27,404 +61,39 @@ export default function Home() {
         <table className={Prog_Styles.tableData}>
             <tbody>
                 <tr className={Prog_Styles.titlesRow}>
-                    <td>Lunes A Viernes</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>Lunes a Viernes</td>
+                    <td><img src='/mk_logo_blue.png'/></td>
                     <td>Sábados y Domingos</td>
                 </tr>
                 
                 {
                 tableAMPM === false?
-
                 <>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Nivel X</td>
-                        <td></td>
-                        <td>00:00</td>
-                        <td></td>
-                        <td>Nivel X</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Detective Conan</td>
-                        <td></td>
-                        <td>00:30</td>
-                        <td></td>
-                        <td>Zenki</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>El Mundo de Beakman</td>
-                        <td></td>
-                        <td>01:00</td>
-                        <td></td>
-                        <td>Estan Arrestados</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Mikami la Cazafantasmas</td>
-                        <td></td>
-                        <td>01:30</td>
-                        <td></td>
-                        <td>Pelswick</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Slam Dunk</td>
-                        <td></td>
-                        <td>02:00</td>
-                        <td></td>
-                        <td>Koni Chan</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Ranma 1/2</td>
-                        <td></td>
-                        <td>02:30</td>
-                        <td></td>
-                        <td>Transformers Beast Wars</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Sailor Moon</td>
-                        <td></td>
-                        <td>03:00</td>
-                        <td></td>
-                        <td>Detective Conan</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Zenki</td>
-                        <td></td>
-                        <td>03:30</td>
-                        <td></td>
-                        <td>Mikami la Cazafantasmas</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Estan Arrestados</td>
-                        <td></td>
-                        <td>04:00</td>
-                        <td></td>
-                        <td>Slam Dunk</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Sakura Card Captor</td>
-                        <td></td>
-                        <td>04:30</td>
-                        <td></td>
-                        <td>Sakura Card Captor</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Transformers Beast Wars</td>
-                        <td></td>
-                        <td>05:00</td>
-                        <td></td>
-                        <td>Ranma 1/2</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>A Jugar con Hugo</td>
-                        <td></td>
-                        <td>05:30</td>
-                        <td></td>
-                        <td>A Jugar con Hugo</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Nivel X</td>
-                        <td></td>
-                        <td>06:00</td>
-                        <td></td>
-                        <td>Nivel X</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Kito Pizzas</td>
-                        <td></td>
-                        <td>06:30</td>
-                        <td></td>
-                        <td>Kito Pizzas</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Mikami la Cazafantasmas</td>
-                        <td></td>
-                        <td>07:00</td>
-                        <td></td>
-                        <td>Mikami la Cazafantasmas</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Doraemon</td>
-                        <td></td>
-                        <td>07:30</td>
-                        <td></td>
-                        <td>Doraemon</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Dragon Ball Z</td>
-                        <td></td>
-                        <td>08:00</td>
-                        <td></td>
-                        <td>Dragon Ball Z</td>
-                    </tr>
-                    
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Los Caballeros del Zodiaco</td>
-                        <td></td>
-                        <td>08:30</td>
-                        <td></td>
-                        <td>Los Caballeros del Zodiaco</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Ranma 1/2</td>
-                        <td></td>
-                        <td>09:00</td>
-                        <td></td>
-                        <td>Ranma 1/2</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Sakura Card Captor</td>
-                        <td></td>
-                        <td>09:30</td>
-                        <td></td>
-                        <td>El Mundo de Beakman</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Koni Chan</td>
-                        <td></td>
-                        <td>10:00</td>
-                        <td></td>
-                        <td>Pelswick</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Slam Dunk</td>
-                        <td></td>
-                        <td>10:30</td>
-                        <td></td>
-                        <td>Slam Dunk</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Nivel X</td>
-                        <td></td>
-                        <td>11:00</td>
-                        <td></td>
-                        <td>Transformers Beast Wars</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>A Jugar Con Hugo</td>
-                        <td></td>
-                        <td>11:30</td>
-                        <td></td>
-                        <td>Kito Pizzas</td>
-                    </tr>
-                
+                {
+                    ListAM.map(serie => (
+                        <tr key={serie.id} className={`${serie.tiempo === rightNow? "liveNow" : ""}`}>
+                        <td>{serie.nombre}</td>
+                        
+                        <td>{serie.tiempo}</td>
+                        
+                        <td>{serie.nombre_weekend}</td>
+                        </tr>
+                    ))
+                }
                 </>
                 :
                 <>
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Kito Pizzas</td>
-                        <td></td>
-                        <td>12:00</td>
-                        <td></td>
-                        <td>A Jugar con Hugo</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Zenki</td>
-                        <td></td>
-                        <td>12:30</td>
-                        <td></td>
-                        <td>Zenki</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Dragon Ball Z</td>
-                        <td></td>
-                        <td>13:00</td>
-                        <td></td>
-                        <td>Dragon Ball Z</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Sailor Moon</td>
-                        <td></td>
-                        <td>13:30</td>
-                        <td></td>
-                        <td>Estan Arrestados</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>El Mundo de Beakman</td>
-                        <td></td>
-                        <td>14:00</td>
-                        <td></td>
-                        <td>El Mundo de Beakman</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Koni Chan</td>
-                        <td></td>
-                        <td>14:30</td>
-                        <td></td>
-                        <td>Doraemon</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Zona Virtual</td>
-                        <td></td>
-                        <td>15:00</td>
-                        <td></td>
-                        <td>Zona Virtual</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Detective Conan</td>
-                        <td></td>
-                        <td>15:30</td>
-                        <td></td>
-                        <td>Mikami la Cazafantasmas</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Transformers Beast Wars</td>
-                        <td></td>
-                        <td>16:00</td>
-                        <td></td>
-                        <td>Koni Chan</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Los Caballeros del Zodiaco</td>
-                        <td></td>
-                        <td>16:30</td>
-                        <td></td>
-                        <td>Los Caballeros del Zodiaco</td>
-                    </tr>
-                    
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Estan Arrestados</td>
-                        <td></td>
-                        <td>17:00</td>
-                        <td></td>
-                        <td>Estan Arrestados</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Ranma 1/2</td>
-                        <td></td>
-                        <td>17:30</td>
-                        <td></td>
-                        <td>Detective Conan</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Slam Dunk</td>
-                        <td></td>
-                        <td>18:00</td>
-                        <td></td>
-                        <td>Slam Dunk</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Pelswick</td>
-                        <td></td>
-                        <td>18:30</td>
-                        <td></td>
-                        <td>Pelswick</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Sakura Card Captor</td>
-                        <td></td>
-                        <td>19:00</td>
-                        <td></td>
-                        <td>Sakura Card Captor</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>El Mundo de Beakman</td>
-                        <td></td>
-                        <td>19:30</td>
-                        <td></td>
-                        <td>Zona Virtual</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Zenki</td>
-                        <td></td>
-                        <td>20:00</td>
-                        <td></td>
-                        <td>Zenki</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Transformers Beast Wars</td>
-                        <td></td>
-                        <td>20:30</td>
-                        <td></td>
-                        <td>Transformers Beast Wars</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Detective Conan</td>
-                        <td></td>
-                        <td>21:00</td>
-                        <td></td>
-                        <td>Detective Conan</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Dragon Ball Z</td>
-                        <td></td>
-                        <td>21:30</td>
-                        <td></td>
-                        <td>Dragon Ball Z</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Mikami la Cazafantasmas</td>
-                        <td></td>
-                        <td>22:00</td>
-                        <td></td>
-                        <td>Mikami la Cazafantasmas</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Zona Virtual</td>
-                        <td></td>
-                        <td>22:30</td>
-                        <td></td>
-                        <td>Ranma 1/2</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Sailor Moon</td>
-                        <td></td>
-                        <td>23:00</td>
-                        <td></td>
-                        <td>Sailor Moon</td>
-                    </tr>
-
-                    <tr className={Prog_Styles.tableRow}>
-                        <td>Los Caballeros del Zodiaco</td>
-                        <td></td>
-                        <td>23:30</td>
-                        <td></td>
-                        <td>Los Caballeros del Zodiaco</td>
-                    </tr>
+                {
+                    ListPM.map(serie => (
+                        <tr key={serie.id} className={`${serie.tiempo === rightNow? "liveNow" : ""}`}>
+                        <td>{serie.nombre}</td>
+                        
+                        <td>{serie.tiempo}</td>
+                        
+                        <td>{serie.nombre_weekend}</td>
+                        </tr>
+                    ))
+                }
                 </>
                 }
             </tbody>
---
+++ utils/tools.js
@@ -0,0 +1,155 @@
+export const setTheTime = (number) =>{
+    const setMinutes = (hour) =>{
+        //odio esta funcion pero lo mejorare luego
+        switch (hour) {
+            case 1: 
+                return "00:00";
+                break
+            case 2: 
+                return "00:30";
+                break
+            case 3: 
+                return "01:00";
+                break
+            case 4: 
+                return "01:30";
+                break
+            case 5: 
+                return "02:00";
+                break
+            case 6: 
+                return "02:30";
+                break
+            case 7: 
+                return "03:00";
+                break
+            case 8: 
+                return "03:30";
+                break
+            case 9: 
+                return "04:00";
+                break
+            case 10: 
+                return "04:30";
+                break
+            case 11: 
+                return "05:00";
+                break
+            case 12: 
+                return "05:30";
+                break
+            case 13: 
+                return "06:00";
+                break
+            case 14: 
+                return "06:30";
+                break
+            case 15: 
+                return "07:00";
+                break
+            case 16: 
+                return "07:30";
+                break
+            case 17: 
+                return "08:00";
+                break
+            case 18: 
+                return "08:30";
+                break
+            case 19: 
+                return "09:00";
+                break
+            case 20: 
+                return "09:30";
+                break
+            case 21: 
+                return "10:00";
+                break
+            case 22: 
+                return "10:30";
+                break
+            case 23: 
+                return "11:00";
+                break
+            case 24: 
+                return "11:30";
+                break
+            case 25: 
+                return "12:00";
+                break
+            case 26: 
+                return "12:30";
+                break
+            case 27: 
+                return "13:00";
+                break
+            case 28: 
+                return "13:30";
+                break
+            case 29: 
+                return "14:00";
+                break
+            case 30: 
+                return "14:30";
+                break
+            case 31: 
+                return "15:00";
+                break
+            case 32: 
+                return "15:30";
+                break
+            case 33: 
+                return "16:00";
+                break
+            case 34: 
+                return "16:30";
+                break
+            case 35: 
+                return "17:00";
+                break
+            case 36: 
+                return "17:30";
+                break
+            case 37: 
+                return "18:00";
+                break
+            case 38: 
+                return "18:30";
+                break
+            case 39: 
+                return "19:00";
+                break
+            case 40: 
+                return "19:30";
+                break
+            case 41: 
+                return "20:00";
+                break
+            case 42: 
+                return "20:30";
+                break
+            case 43: 
+                return "21:00";
+                break
+            case 44: 
+                return "21:30";
+                break
+            case 45: 
+                return "22:00";
+                break
+            case 46: 
+                return "22:30";
+                break
+            case 47: 
+                return "23:00";
+                break
+            case 48: 
+                return "23:30";
+                break
+            default:
+                break;
+        }
+    }
+
+    return setMinutes(number)
+}
