15dc7520a42df8ba7a6f828f937ef607aceb74c5
Alexander Mamani <alex.rps96@gmail.com>
2022-09-02 11:39:14 -0300

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,8 @@ export default function Document() {
         <meta name="description" content="Revivi la experiencia Magic Kids, Magic Stream es una pagina dedicada al canal magic kids y para recrear su transmision, disfruta de la programacion de magic kids las 24hs del dia todos los dias." />
         <meta property="og:title" content="Magic Stream" />
         <meta property="og:type" content="video.series" />
-        <link rel="preload" href="/fonts/MavenPro-Medium.woff2" as="font" type="font/woff2" crossorigin=""></link>
-        <link rel="preload" href="/fonts/MavenPro-SemiBold.woff2" as="font" type="font/woff2" crossorigin=""></link>
+        <link rel="preload" href="/fonts/MavenPro-Medium.woff2" as="font" type="font/woff2" crossOrigin=""></link>
+        <link rel="preload" href="/fonts/MavenPro-SemiBold.woff2" as="font" type="font/woff2" crossOrigin=""></link>
         <link rel='shortcut icon' href='favicon.ico'></link>
         <link rel='preload' as='image' href='/img/magickidsbg.png'></link>
         <link rel="preload" as="image" href="/icons/facebook.svg"></link>
