377a30b9459850e8a0c0f20df0d486ed2ebd11e9
Alexander Mamani <alex.rps96@gmail.com>
2022-09-18 21:26:56 -0300

    fix

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
                 <a href="https://www.facebook.com/tvmagicstream" target="_blank" rel="noreferrer noopener" title="Seguime en Facebook"><img src='/icons/facebook.svg' alt='icon'/></a>
                 <a href="https://www.instagram.com/magicstreamok" target="_blank" rel="noreferrer noopener" title="Seguime en Instagram"><img src='/icons/instagram.svg' alt='icon'/></a>
                 <a href="https://www.taringa.net/magicstream" target="_blank" rel="noreferrer noopener" title="Seguime en Taringa!"><img src='/icons/taringa.svg' alt='icon'/></a>
-                <a href="https://www.github.com/alexqs96" target="_blank" rel="noreferrer noopener" title="Hola! Gracias por ver mi Web!"><img src='/icons/github.svg' alt='icon'/></a>
+                <a href="https://www.github.com/alexqs96" target="_blank" rel="noreferrer noopener" title="Hola! Gracias por visitar Magic Stream!"><img src='/icons/github.svg' alt='icon'/></a>
             </div>
             <p>En dedicación al Canal Magic Kids</p>
             <p>Magic Stream</p>
