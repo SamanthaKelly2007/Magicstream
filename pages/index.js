5de7ae3ea9c76a403581ba3722f0f6e76e103006
Alexander Mamani <alex.rps96@gmail.com>
2022-09-21 19:20:04 -0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,6 @@ export default function Home() {
 
   const getShowSchedule = () => {
     setShowsNav(getCurrentNextShow(getActualTime()))
-    setShowsNav(getCurrentNextShow('20:00'))
     setMinutes(new Date().getMinutes())
   }
 
